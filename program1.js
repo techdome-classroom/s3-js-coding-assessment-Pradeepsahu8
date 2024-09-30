5710403ae45170fb81a6c63f2f8e2df9132d3d40
Pradeepsahu8 <usesforsomething@gmail.com>
2024-09-30 15:48:19 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -28,7 +28,7 @@ if(bracketMap[char])
 }
 return stack.length === 0;
 console.log(isValid("()"));        // Output: true
-console.log(isValid("()[]{}"));    // Output: true
+console.log(isValid("()[]{}"));    
 console.log(isValid("(]"));   
 };
 
