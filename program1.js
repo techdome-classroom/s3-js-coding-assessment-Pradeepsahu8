ee4dc9a11195f56c8686490ab7747cfd6d55dc13
Pradeepsahu8 <usesforsomething@gmail.com>
2024-09-30 15:49:02 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -30,6 +30,11 @@ return stack.length === 0;
 
 };
 
+console.log(isValid("()"));        
+console.log(isValid("()[]{}"));    
+console.log(isValid("(]"));   
+};
+
 module.exports = { isValid };
 
 
